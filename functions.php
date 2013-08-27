6ee4ef43cba0624c3fe2f55e6d2bf66e96a4b872
Dave Kellam <dave@eightface.com>
2013-08-27 01:01:18 +0100

    Changing function prefix to sl from si (to reflect plugin name)

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function si_surprise_lipsum() {
+function sl_surprise_lipsum() {
 	global $surprise_lipsum;
 	return $surprise_lipsum;
 }
@@ -9,14 +9,14 @@ function si_surprise_lipsum() {
  * Get lipsum
  * @return string A string of lipsum
  */
-function si_get_lipsum() {
+function sl_get_lipsum() {
 	return si_surprise_lipsum()->get_lipsum();
 }
 
 /**
  * Get lipsum and display
  */
-function si_lipsum() {
+function sl_lipsum() {
 	$lipsum = si_surprise_lipsum()->get_lipsum();
 	echo $lipsum;
 }
