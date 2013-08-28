dfeb7bc603a52dd60f126f85146f1acaa6980434
Dave Kellam <dave@eightface.com>
2013-08-28 17:39:37 -0400

    Adding word list and basic lipsum with variance

--- surprise-lipsum.php
+++ surprise-lipsum.php
@@ -20,7 +20,111 @@ require_once( SURPRISE_LIPSUM_PATH . '/functions.php' );
 
 class Surprise_Lipsum {
 	function __construct() {
-		// May not need this, here for now
+		$this->words = array( 
+			'lorem',
+			'ipsum',
+			'dolor',
+			'sit',
+			'amet',
+			'consectetuer',
+			'adipiscing',
+			'elit',
+			'nam',
+			'cursus',
+			'morbi',
+			'ut',
+			'mi',
+			'nullam',
+			'enim',
+			'leo',
+			'egestas',
+			'id',
+			'condimentum',
+			'at',
+			'laoreet',
+			'mattis',
+			'massa',
+			'sed',
+			'eleifend',
+			'nonummy',
+			'diam',
+			'praesent',
+			'mauris',
+			'ante',
+			'elementum',
+			'et',
+			'bibendum',
+			'at',
+			'posuere',
+			'sit',
+			'amet',
+			'nibh',
+			'duis',
+			'tincidunt',
+			'lectus',
+			'quis',
+			'dui',
+			'viverra',
+			'vestibulum',
+			'suspendisse',
+			'vulputate',
+			'aliquam',
+			'dui',
+			'nulla',
+			'elementum',
+			'dui',
+			'ut',
+			'augue',
+			'aliquam',
+			'vehicula',
+			'mi',
+			'at',
+			'mauris',
+			'maecenas',
+			'placerat',
+			'nisl',
+			'at',
+			'consequat',
+			'rhoncus',
+			'sem',
+			'nunc',
+			'gravida',
+			'justo',
+			'quis',
+			'eleifend',
+			'arcu',
+			'velit',
+			'quis',
+			'lacus',
+			'morbi',
+			'magna',
+			'magna',
+			'tincidunt',
+			'a',
+			'mattis',
+			'non',
+			'imperdiet',
+			'vitae',
+			'tellus',
+			'sed',
+			'odio',
+			'est',
+			'auctor',
+			'ac',
+			'sollicitudin',
+			'in',
+			'consequat',
+			'vitae',
+			'orci',
+			'fusce',
+			'id',
+			'felis',
+			'vivamus',
+			'sollicitudin',
+			'metus',
+			'eget',
+			'eros'
+		);
 	}
 
 	function get_lipsum() {
@@ -28,8 +132,28 @@ class Surprise_Lipsum {
 		return $lipsum;
 	}
 
-	function _generate_lipsum() {
-		return "Lorem ipsum";
+	function _generate_lipsum( $num = 50 ) {
+		$count = ''; 
+		$sentence = '';
+
+		$num = $this->_lipsum_variance( $num ); echo $num;
+
+		while ( $count < $num ) {
+			$word = array_rand( $this->words );
+			$sentence .= $this->words[$word] . ' ';
+			$count++;
+		}
+
+		return $sentence;
+	}
+
+	function _lipsum_variance( $num, $variance = 0.2 ) {
+		$num_low = $num - $num * $variance;
+		$num_high = $num + $num * $variance;
+
+		$new_num = rand( (int) $num_low, (int) $num_high );
+
+		return $new_num;
 	}
 }
 
