e90aae2d1333776494615830b8e092058c1fa2e8
Dave Kellam <dave@eightface.com>
2013-09-30 23:19:13 -0400

    Adding test to check that variance is an integer

--- tests/test-functions.php
+++ tests/test-functions.php
@@ -5,4 +5,8 @@ class Tests_Functions extends WP_UnitTestCase {
 	function test_get_lipsum_is_string() {
 		$this->assertTrue( is_string( sl_get_lipsum() ) );
 	}
+
+	function test_variance_is_int() {
+		$this->assertTrue( is_int( Surprise_Lipsum::_variance( 50 ) ) );
+	}
 }
